3e33401032eeea85f69f7cae0812a88dc7cfca0a
Vincent Fleurette <mail@iserv.fr>
2025-05-13 14:38:51 +0200

    Initial Commit

---
+++ OddoTests/OddoTests.swift
@@ -0,0 +1,17 @@
+//
+//  OddoTests.swift
+//  OddoTests
+//
+//  Created by Vincent Fleurette on 13/05/2025.
+//
+
+import Testing
+@testable import Oddo
+
+struct OddoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
